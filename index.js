79417c99865d736312255c5a4a94d36ea0c17c33
Ivanliu101 <ivanliu101@gmail.com>
2025-09-19 07:32:51 +0800

    Beta 2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+{
+    name: "任務名稱",
+        due: "2025/09/19 12:00",
+    tags: ["總結性評量", "作業"],
+    subjects: ["英文", "數學"],
+    completed: false,
+    late: 0
+}
